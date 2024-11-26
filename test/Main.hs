747e018a8589229972418594ea96ca8dca9fe7ec
Eugene Samodelov <geli1610@yandex.ru>
2024-11-26 16:21:36 +0300

    fix: add test

--- test/Main.hs
+++ test/Main.hs
@@ -1,6 +1,16 @@
 module Main (main) where
 
-import Prelude (IO, putStrLn)
+import           Prelude                                  (Bool (True), IO, ($))
+import           Server                                   (Context, InputData, K, N, PlonkKYC, kycData, verifyKYCData)
+import           Test.Tasty                               (defaultMain)
+import           Test.Tasty.HUnit                         (assertEqual, testCase)
+
+import           ZkFold.Base.Protocol.NonInteractiveProof (HaskellCore, verify)
+import           ZkFold.Prelude                           (readFileJSON)
 
 main :: IO ()
-main = putStrLn "Test suite not yet implemented."
+main = defaultMain $
+    testCase "KYC example test case" $ do
+    json <- readFileJSON "example-json/kyc-data.json" :: IO (InputData N K Context)
+    let !(s, i, p) = verifyKYCData (kycData json)
+    assertEqual "Verify result must be true" True $ verify @(PlonkKYC 29 272) @HaskellCore s i p
